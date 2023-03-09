6dce26db0800510b9ece77516c58c7dac35db6f1
txcalon <txc.alon@gmail.com>
2023-03-09 09:59:33 +0800

    Chapter7 Homework 1

---
+++ 01-Notes/04-CPP_Primer_5th_Homework/Testcode/Chapter7 Class/Person.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Person
+{
+private:
+    string strName;
+    string strAddress;
+
+public:
+    Person() = default;
+    Person(const string &_strName, const string &_strAddress) : strName(_strName), strAddress(_strAddress) {}
+    istream &operator>>(istream &is, Person &p)
+    {
+        is >> p.strName >> p.strAddress;
+        return is;
+    }
+    Person(std::istream &is) { is >> *this; }
+    string getName() const { return strName; }
+    string getAddress() const { return strAddress; }
+    void PersonPrint() { cout << "Personname: " << strName << "\nPersonaddress: " << strAddress << endl; }
+};
+int main()
+{
+    Person She = Person("Shiyi", "shanghai");
+    She.PersonPrint();
+}
